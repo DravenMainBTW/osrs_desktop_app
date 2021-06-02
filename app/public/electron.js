b75f602c6a5e69a361c6c935d3e585d86c0be1d4
Daniel <danielcrewe1999@gmail.com>
2021-06-03 00:21:28 +0100

    setting up ability to build app

--- app/public/electron.js
+++ app/public/electron.js
@@ -1,19 +1,30 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 400,
+    minWidth: 400,
     maxWidth: 400,
     height: 600,
+    minHeight: 600,
     maxHeight: 600,
     // frame: false,
-    icon: __dirname + "/public/icon.ico",
+    icon: __dirname + "/cash_stack.ico",
   });
 
-  mainWindow.loadURL("http://localhost:3000");
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 }
 
 app.whenReady().then(() => {
--- app/src/components/homepage/index.js
+++ app/src/components/homepage/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Typography } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 
 export default class index extends Component {
   render() {

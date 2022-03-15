b76739be7671352c69eab67e377a1a4fd0335cd7
Daniel <danielcrewe1999@gmail.com>
2022-03-15 22:39:13 +0000

    Adding comments to the backend to explain code

--- server/src/database_start.js
+++ server/src/database_start.js
@@ -4,16 +4,18 @@ import cors from "cors";
 import routes from "./routes";
 import CronManager from "./crons";
 
+// FOR TESTING
 const mongoDB = "mongodb://localhost:27017/OSRS_GE_APP";
-// TEMP FOR TESTING
 const corsOptions = {
   origin: "http://localhost:3000",
 };
 
+// Conection
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
+// On error
 db.on("error", () => {
   console.log("###########################################");
   console.log("# Error Encountered While Starting Server #");
@@ -23,11 +25,16 @@ db.on("error", () => {
 db.once("open", async () => {
   const app = express();
 
+  // Setting up CORS to allow frontend to communicate
   app.use(cors(corsOptions));
+
+  // JSON parsing middleware
   app.use(express.json());
 
+  // Adding routes on to "/api"
   app.use("/api", routes);
 
+  // Setting up crons
   await CronManager.setup(app);
 
   app.listen(5000, () => {
--- server/src/routes/items/list.js
+++ server/src/routes/items/list.js
@@ -1,5 +1,6 @@
 import { Item } from "../../models";
 
+// Lists all items
 export default async (req, res) => {
   const items = await Item.find();
 
